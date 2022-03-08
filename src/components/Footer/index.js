210db73ae7bd8a50cd6bdc490e41f834e5621537
santoadji21 <ahmad.aji.santoso21@gmail.com>
2022-03-08 12:53:33 +0700

    fix filter button

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,72 @@
 import React from "react";
 
 export default function Footer({ filter }) {
+  const [allbuttonfilter, setAllButtonfilter] = React.useState("");
+  const [activeButtonfilter, setActiveButtonfilter] = React.useState("");
+  const [completedButtonfilter, setCompletedButtonfilter] = React.useState("");
+
+  const buttonColor = (e) => {
+    if (e.target.value === "ALL") {
+      setAllButtonfilter("filter-all");
+      setActiveButtonfilter("");
+      setCompletedButtonfilter("");
+    } else if (e.target.value === "ACTIVE") {
+      setAllButtonfilter("");
+      setActiveButtonfilter("filter-active");
+      setCompletedButtonfilter("");
+    } else if (e.target.value === "COMPLETED") {
+      setAllButtonfilter("");
+      setActiveButtonfilter("");
+      setCompletedButtonfilter("filter-completed");
+    }
+    // console.log(allbuttonfilter);
+  };
+
+  const allFilterHandler = (e) => {
+    buttonColor(e)
+    filter("ALL")
+  }
+  const activeFilterHandler = (e) => {
+    buttonColor(e)
+    filter("ACTIVE")
+  }
+  const completedFilterHandler = (e) => {
+    buttonColor(e)
+    filter("COMPLETED")
+  }
 
   return (
     <>
       <div className="button-filter">
-        <button onClick={() => filter("ACTIVE")}>
+        <button
+          value="ALL"
+          className={allbuttonfilter}
+          onClick={allFilterHandler}
+        >
           ALL
         </button>
-        <button onClick={() => filter("ACTIVE")}>ACTIVE</button>
-        <button onClick={() => filter("COMPLETED")}>COMPLETED</button>
+        <button
+          value="ACTIVE"
+          className={activeButtonfilter}
+          onClick={activeFilterHandler}
+        >
+          ACTIVE
+        </button>
+        <button
+          value="COMPLETED"
+          className={completedButtonfilter}
+          onClick={completedFilterHandler}
+        >
+          COMPLETED
+        </button>
       </div>
+      {/* <div className="button-filter">
+        <button value="ALL" className={buttoncolor} onClick={allFilters}>
+          ALL
+        </button>
+        <button value="ACTIVE" onClick={() => filter("ACTIVE")}>ACTIVE</button>
+        <button value="COMPLETED" onClick={() => filter("COMPLETED")}>COMPLETED</button>
+      </div> */}
     </>
   );
 }
